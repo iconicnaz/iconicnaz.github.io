31cb5715ed84ce1bd8526a4e67d50bd5717d5800
iconicnaz <35021235+iconicnaz@users.noreply.github.com>
2018-01-02 01:15:39 -0500

    Attempt at fixing PHP Form #1

--- scripts/form-u2391.php
+++ scripts/form-u2391.php
@@ -27,8 +27,8 @@ $form = array(
 		'unknown_method' => 'Unknown server request method'
 	),
 	'email' => array(
-		'from' => 'iconicnaz@gmail.com',
-		'to' => 'iconicnaz@gmail.com'
+		'from' => 'official@iconicnaz.com',
+		'to' => 'official@iconicnaz.com'
 	),
 	'fields' => array(
 		'custom_U2392' => array(
